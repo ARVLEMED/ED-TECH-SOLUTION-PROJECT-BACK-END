d61d35359423ad5a64167fbacb2e53430af77f5d
Basil Wabuke <babazwabuke4@gmail.com>
2025-03-01 07:24:59 +0300

    ETSA-12 adding flask-cors

--- app/__init__.py
+++ app/__init__.py
@@ -3,6 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_bcrypt import Bcrypt
 from flask_marshmallow import Marshmallow
+from flask_cors import CORS
 
 db = SQLAlchemy()
 migrate = Migrate()
@@ -17,6 +18,7 @@ def create_app(config_class="app.config.Config"):
     migrate.init_app(app, db)
     bcrypt.init_app(app)
     ma.init_app(app)
+    CORS(app)
 
     from app.routes import api_bp
     app.register_blueprint(api_bp)
