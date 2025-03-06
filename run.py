aa5246b66cb0692dd6fd1c7c4f162eafa4eaf56f
aron k koech <varokoaruna81@gmail.com>
2025-03-07 02:12:40 +0300

    edits run.py

--- run.py
+++ run.py
@@ -1,14 +1,10 @@
 from app import create_app
 from seed_data import seed_data
 
-app = create_app()
+app = create_app()  # Define `app` at the module level
 
-# Run the seed function inside the app context
 if __name__ == '__main__':
     with app.app_context():
-        # Seed the database (you can choose to remove this line if you want to seed manually)
         seed_data()
         print("Mock data seeded!")
-
-    # Start the Flask application
     app.run(debug=True)
